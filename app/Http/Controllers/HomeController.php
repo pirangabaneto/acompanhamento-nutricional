9e62098a75b2200d222abee2835ad4a3dda925a8
José Pirangaba <pirangabaneto@gmail.com>
2021-10-24 20:59:49 -0300

    adicionando diferentes perfis de login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if(Auth::user()->tipo == 0)
+            return view('home');
+        return view('homePaciente');
+        //return view('home');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,17 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="tipo" class="col-md-4 col-form-label text-md-right">{{ "Perfil" }}</label>
+
+                            <div class="col-md-6">
+                                <select id="perfil" class="selectpicker" name="tipo" required>
+                                    <option>Nutricionista</option>
+                                    <option>Paciente</option>
+                                </select>
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
